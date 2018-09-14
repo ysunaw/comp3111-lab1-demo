1af873676725d29497af9c1f8f356867ac736724
ysunaw <ysunaw@csl4wk47.win2k.cs.ust.hk>
2018-09-14 09:41:41 +0800

    Change in Library.java

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,7 +3,9 @@
  */
 public class Library {
     public static void main(String arg[]) {
-    	System.out.println("When there is a fire, commit and push.");
+    	Library lib = new Library();
+    	if(lib.someLibraryMethod())
+    		System.out.println("When there is a fire, commit and push.");
     }
 	public boolean someLibraryMethod() {
         return true;
